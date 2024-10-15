c3e3b9d5c683ac5fcdecc9e77ddedfdd8e459dca
moscolape <chunkums088@gmail.com>
2024-10-15 12:51:10 +0100

    chore: hide api key

--- api/authenticate.js
+++ api/authenticate.js
@@ -1,9 +1,13 @@
-import axios from "axios";
+import axios from 'axios';
+import Constants from 'expo-constants';
 
-// @ts-ignore
-const API_KEY = import.meta.env.FIREBASE_API_KEY;
+const API_KEY = Constants.expoConfig?.extra?.firebaseApiKey;
 
 async function authenticate(mode, email, password) {
+  if (!API_KEY) {
+    throw new Error('Firebase API key is missing');
+  }
+
   const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
 
   const response = await axios.post(url, {
@@ -13,7 +17,6 @@ async function authenticate(mode, email, password) {
   });
 
   const token = response.data.idToken;
-
   return token;
 }
 
