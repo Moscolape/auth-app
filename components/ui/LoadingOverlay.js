f1793c1c9ef837fc3ddc3ce2c91c1923718864d2
moscolape <chunkums088@gmail.com>
2024-10-15 11:50:05 +0100

    chore: push all files

---
+++ components/ui/LoadingOverlay.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
+import { Colors } from '../../constants/styles';
+
+function LoadingOverlay({ message }) {
+  return (
+    <View style={styles.rootContainer}>
+      <Text style={styles.message}>{message}</Text>
+      <ActivityIndicator size="large" color={Colors.primary800}/>
+    </View>
+  );
+}
+
+export default LoadingOverlay;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 32,
+  },
+  message: {
+    fontSize: 16,
+    marginBottom: 12,
+  },
+});
